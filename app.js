eba5232e8a09e9ff1770a9c207a6f96aaa67646d
ThomasElkins <elkins.thomas@gmail.com>
2017-07-05 14:17:01 -0700

    added Ralph gif

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ console.log("Javascript is alive!");
 document.getElementById("greeting").textContent="Hello, World!"
 
 document.getElementById("essentials").style.backgroundColor="yellow"
+
+var ralphGif = document.createElement("img")
+ralphGif.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif"
+
+document.getElementById("greeting").append(ralphGif)
