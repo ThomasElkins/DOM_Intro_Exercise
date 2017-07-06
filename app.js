840a1b9908ae97833ff3d4f487a245b4838a0f74
ThomasElkins <elkins.thomas@gmail.com>
2017-07-06 11:10:45 -0700

    now with jqeury

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-console.log("Javascript is alive!");
+$("#greeting").text("Hello, World!");
 
-document.getElementById("greeting").textContent="Hello, World!"
+$("#essentials").css("backgroundColor", "yellow");
 
-document.getElementById("essentials").style.backgroundColor="yellow"
-
-var ralphGif = document.createElement("img")
-ralphGif.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif"
-
-document.getElementById("greeting").append(ralphGif)
+$("#greeting").append('<img src="http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif">');
