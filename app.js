5c58cb8d0102748082078f541709b440f7f8fbe3
ThomasElkins <elkins.thomas@gmail.com>
2017-07-06 13:38:11 -0700

    major progress

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-$("#greeting").text("Hello, World!");
+$('#greeting').text("Hello, World!");
 
-$("#essentials").css("backgroundColor", "yellow");
+$('#essentials').css('backgroundColor', 'yellow');
 
-$("#greeting").append('<img src="http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif">');
+$('#greeting').append('<img src="http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif">');
+
+$('#essentials li').click(function() {
+  var imgText = $(this).text()
+  var imgTarget = "./images/" + imgText + ".jpeg"
+    $('.selected').removeClass();
+    $(this).toggleClass('selected');
+    $('img[alt="beer"]').attr("src", imgTarget)
+})
+
+$('#ghosting').hover(function(){
+  $(this).css('opacity', '0')
+}, function(){
+  $(this).css('opacity', '1')
+})
