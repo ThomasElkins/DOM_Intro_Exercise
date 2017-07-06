ac2c956d1d6531209aeae27bcee57f6e6eb63a65
ThomasElkins <elkins.thomas@gmail.com>
2017-07-06 14:42:46 -0700

    all exercises completed

--- app.js
+++ app.js
@@ -17,3 +17,58 @@ $('#ghosting').hover(function(){
 }, function(){
   $(this).css('opacity', '1')
 })
+
+$('#resize').hover(function(){
+  $(this).css('width', '400px')
+}, function(){
+  $(this).css('width', '200px')
+})
+
+$('#reset').click(function(){
+  $('.selected').removeClass();
+  $('img[alt="beer"]').attr("src", "./images/panic.jpeg")
+})
+
+$(document).keypress(function(){
+  console.log(event.which)
+  if (event.which >= 48 && event.which <= 57){
+    alert("I HATE NUMBERZZZ")
+  }
+})
+
+
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+neededkeys = [38,38,40,40,37,39,37,39,66,65], started = false, count = 0;
+$(document).keydown(function(e) {
+    key = e.keyCode;
+    if (!started) {
+        if (key == 38) {
+            started = true;
+        }
+    }
+    if (started) {
+        if (neededkeys[count] == key) {
+            count++;
+        } else {
+            reset();
+        }
+        if (count == 10) {
+            reset();
+            alert('Cheat Codes Activated');    
+        }
+    } else {
+        reset();
+    }
+});
+function reset() {
+    started = false;
+    count = 0;
+}
